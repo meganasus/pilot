e1b7e46d870d14c1e5c0266780bf5bc4ad0ebee8
meganasus <crisgarc06@outlook.com>
2025-06-15 18:42:02 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-const menuH2 = document.querySelector(".menu h2");
-const menuH3 = document.querySelector("menu h3");
-menuH2.addEventListener("click", () => {
-  if (menuH3.classList.value.includes("hidden")) menuH3.classList.remove("hidden");
-  else menuH3.classList.add("hidden");
-})
+// const menuH2 = document.querySelector(".menu h2");
+// const menuH3 = document.querySelector("menu h3");
+// menuH2.addEventListener("click", () => {
+//   if (menuH3.classList.value.includes("hidden")) menuH3.classList.remove("hidden");
+//   else menuH3.classList.add("hidden");
+// })
