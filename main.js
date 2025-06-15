d120df0404070ff82aa7aa1a4bc925ab3a566a23
meganasus <crisgarc06@outlook.com>
2025-06-15 18:35:41 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const menuH3 = document.querySelector(".menu h3");
+const menuH3 = document.querySelector(".menu .hidden h3");
 menuH3.addEventListener("click", () => {
   if (menuH3.classList.value.includes("hidden")) menuH3.classList.remove("hidden");
   else menuH3.classList.add("hidden");
