d2b0be43fcbe61105ace8d750aa2ae6142afc675
meganasus <crisgarc06@outlook.com>
2025-06-22 17:52:58 -0400

    Update main.js

--- main.js
+++ main.js
@@ -2,5 +2,6 @@ const mobileImg = document.querySelector("img.background-mobil");
 const screenWidth = window.screen.width;
 if (screenWidth <= 1000) {
     mobileImg.classList.remove("hidden");
-    document.querySelector("img.background").classList.value.add("hidden");
+    const backgroundImg = document.querySelector("img.background");
+    backgroundImg.classList.add("hidden");
 }
