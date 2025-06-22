b76e122ec52d7d1a17c20cd7de5114c04c2d7647
meganasus <crisgarc06@outlook.com>
2025-06-22 05:04:37 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const mobileImg = document.querySelector("img.mobil-background");
-if (mobileImg) {
-    
+if (window.screen.width <= 1000) {
+    mobilImg.classList.remove("hidden");
+    document.querySelector("img.background").classList.add("hidden");
 }
