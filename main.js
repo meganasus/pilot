38f7cedf8cfc53e290a3916f2d43ea9993b18f41
meganasus <crisgarcc1@outlook.com>
2025-06-24 17:55:58 -0400

    Update main.js

--- main.js
+++ main.js
@@ -4,4 +4,6 @@ if (screenWidth <= 1000) {
   const mainIndexElt = document.querySelector("main.menu-contact large");
   mainIndexElt.classList.remove("large");
   console.log("Large Class Removed");
+} else {
+  console.log("large screen");
 }
