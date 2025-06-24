cc99cd00342888f662738cb16d30911555d20705
meganasus <crisgarcc1@outlook.com>
2025-06-24 17:10:20 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,1 @@
-// const mobileImg = document.querySelector("img.background-mobil");
-// const screenWidth = screen.width;
-// if (screenWidth <= 1000) {
-//     mobileImg.classList.remove("hidden");
-// }
+
