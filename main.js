1c6ff4ca776d20113aa215cfe8bc6a05200fb55e
meganasus <crisgarcc2@gmail.com>
2025-06-27 01:13:37 -0400

    Last Sync: 2025-06-27 01:13 (Mobile)

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const screenWidth = screen.width;
 
 if (screenWidth <= 1000) {
-  const mainIndexElt = document.querySelector("main.menu-contact large");
+  const mainIndexElt = document.querySelector("main.menu-contact.large");
   mainIndexElt.classList.remove("large");
   console.log("Large Class Removed");
 } else {
