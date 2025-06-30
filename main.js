24791badc06fb544217a09da25c7921175f90a36
meganasus <crisgarcc1@outlook.com>
2025-06-30 18:39:40 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 const screenWidth = window.screen.width;
+let screen = "desktop";
 
 if (screenWidth <= 1000) {
   const mainIndexElt = document.querySelector("section.menu-contact-logo.large");
   mainIndexElt.classList.remove("large");
   console.log("Large Class Removed");
+  screen = "mobile";
 } else {
   console.log("large screen");
 }
+
